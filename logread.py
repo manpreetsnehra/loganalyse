460664710a5c9857c8b67833f70ce2203344ebfb
Manpreet Singh Nehra <manpreet.nehra@tolexo.com>
2016-04-30 23:26:36 +0530

    split datetime into date and time columns)

--- logread.py
+++ logread.py
@@ -12,21 +12,22 @@ def read(data):
     for line in logfile:
         try:
             record = re.search('([\d+\.]+) - - \[(.*?)\] "(.*?)" (\d+) (.*?) "(.*?)" "(.*?)"',line).groups()
-            (time,zone) = string.split(record[1])
+            (datetime,zone) = string.split(record[1])
             (command,request,responseproto) = string.split(record[2])
+            (date,time)=string.split(datetime,":",1)
         except:
             continue
-        values=(record[0],time,int(zone),command,request,responseproto,int(record[3]),record[4],record[5],record[6],)
+        values=(record[0],date,time,int(zone),command,request,responseproto,int(record[3]),record[4],record[5],record[6],)
         logsrecord=re.match("^/logs/(.*)",request)
         if logsrecord == None:        
-            cur.execute('''INSERT OR IGNORE INTO Logs (remote_user,time,timezone,command,request,responseproto,status,bytes_sent,referer,ua,processed) VALUES(?,?,?,?,?,?,?,?,?,?,0)''',values )
+            cur.execute('''INSERT OR IGNORE INTO Logs (remote_user,date,time,timezone,command,request,responseproto,status,bytes_sent,referer,ua,processed) VALUES(?,?,?,?,?,?,?,?,?,?,?,0)''',values )
     logfile.close()
     
     
 conn = sqlite3.connect('accesslogs.sqlite')
 cur = conn.cursor()
 #create table for all data from files to SQL
-cur.execute('''CREATE TABLE IF NOT EXISTS Logs (id INTEGER PRIMARY KEY AUTOINCREMENT, remote_user TEXT , time TEXT, timezone INTEGER, command TEXT, request TEXT, responseproto TEXT,status INTEGER, bytes_sent TEXT, referer TEXT,ua TEXT,processed INTEGER)''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Logs (id INTEGER PRIMARY KEY AUTOINCREMENT, remote_user TEXT , date TEXT,time TEXT, timezone INTEGER, command TEXT, request TEXT, responseproto TEXT,status INTEGER, bytes_sent TEXT, referer TEXT,ua TEXT,processed INTEGER)''')
 cur.execute('''CREATE TABLE IF NOT EXISTS LogFiles (id INTEGER PRIMARY KEY AUTOINCREMENT, fileuri TEXT, status INTEGER)''')
                 
 parser = argparse.ArgumentParser(description="Read Apache Logs and push into sqlite")
